8dc1d7c2fa837ebb5954491e754d24ac8100e756
Paul Suh <phs92@cornell.edu>
2021-10-16 23:31:30 -0400

    demo gui

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
+open Graphics
+
+let () = open_graph ""
+
+let () = set_window_title "Hello"
+
+let () = draw_string "Hello"
+
 let () = print_endline "Hello, World!"
+
+let x = wait_next_event [Key_pressed]
