4915b14bd35c331054d69b9ba565a1e1ce568cc4
ridhitbhura <rb749@cornell.edu>
2021-10-16 21:08:56 -0400

    author files

---
+++ src/author.mli
@@ -0,0 +1,5 @@
+(** CS 3110 Fall 2021 Assignment A2 Adventure
+    @author Ridhit Bhura (rb749) *)
+
+ val hours_worked : int
+ (** [hours_worked] is the number of hours you worked on this assignment. *)
