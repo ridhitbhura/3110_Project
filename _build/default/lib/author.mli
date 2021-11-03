171ef4a8d4c2a58d4df9d113826ad99b16de457b
kabirwalia8300 <kabirwalia55@gmail.com>
2021-11-03 18:45:04 -0400

    fixing gitignore - no build

--- _build/default/lib/author.mli
+++
@@ -1,8 +0,0 @@
-(** CS 3110 Fall 2021 Final Project
-    @author Ridhit Bhura (rb749) 
-    @author Paul Suh (phs92) 
-    @author Kenny Liang (ql75) 
-    @author Kabir Walia (ksw63) *)
-
- val hours_worked : int
- (** [hours_worked] is the number of hours you worked on this assignment. *)
